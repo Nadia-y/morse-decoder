827015522880e1aee58c33b4ec3b82f47aec9739
Nadia-y <49363012+Nadia-y@users.noreply.github.com>
2019-10-07 18:19:52 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -39,8 +39,42 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    var vernem="";
+    while(expr.length!=0)
+    {
+        tempV=expr.slice(0,10);
+        expr=expr.substring(10);
+        if(tempV=="**********")
+        {
+            vernem+=" ";
+        }
+        else
+        {
+            vernem+=toMors(tempV);
+        }
+    }
+    return vernem;
 }
 
+function toMors(numC)
+{
+    var converted="";
+    while(numC.length!=0)
+    {
+        tempV=numC.slice(0,2);
+        numC=numC.substring(2);
+        if(tempV=="10")
+        {
+            converted+=".";
+        }
+        else if(tempV=="11")
+        {
+            converted+="-";
+        }
+    }
+    converted=MORSE_TABLE[converted];
+    return converted;
+}
 module.exports = {
     decode
 }
